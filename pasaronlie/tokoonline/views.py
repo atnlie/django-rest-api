d07ee47e8e8fa709ef936d65249831d0132451a4
Anton <anton@MacBook-Pro-Anton.local>
2023-07-22 10:19:34 +0700

    fix(responseCode): use respose code from rest_framework status

--- pasaronlie/tokoonline/views.py
+++ pasaronlie/tokoonline/views.py
@@ -1,27 +1,32 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .serializer import ProductSerializer
 from .models import Product
- 
+
+@csrf_exempt
 @api_view(['GET'])
 def getProduct(request):
     return Response()
 
+@csrf_exempt
 @api_view(['GET'])
 def getAllProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
+@csrf_exempt
 @api_view(['POST'])
 def addProduct(request):
     serializer = ProductSerializer(data=request.data)
     if(serializer.is_valid()):
         serializer.save()
         result = {
-            'code': 200,
+            'code': status.HTTP_200_OK,
             'message': 'success',
             'data': serializer.data,
             'error': null
@@ -29,7 +34,7 @@ def addProduct(request):
         return Response(result)
     else:
         result = {
-            'code': 400,
+            'code': status.HTTP_400_BAD_REQUEST,
             'message': 'error',
             'data': [],
             'error': serializer.errors
