287f1ffe1f253cb533e98ef70588cc05c6af1610
Anton <anton@MacBook-Pro-Anton.local>
2023-07-23 08:34:44 +0700

    feat(clasBasedView): add new approach using class based

--- pasaronlie/tokoonline/urls.py
+++ pasaronlie/tokoonline/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from . import views
 from django.conf import settings
- 
+# from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
 urlpatterns = [
     path('', views.getProduct, name='getProduct'),
     path('get-all/', views.getAllProducts, name='getAllProduct'),
     path('add-product/', views.addProduct, name='addProduct'),
     path('product/', views.product, name='product'),
+    path('product-list/', views.ProductList.as_view(), name='product-list'),
+    path('product-list/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
 ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- pasaronlie/tokoonline/views.py
+++ pasaronlie/tokoonline/views.py
@@ -1,9 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.views import APIView
 from .serializer import ProductSerializer
 from .models import Product
 
@@ -21,9 +21,9 @@ def getAllProducts(request):
         'code': status.HTTP_200_OK,
         'message': 'success',
         'data': serializer.data,
-        'error': ''
+        'error': False
     }
-    return Response(result)
+    return Response(result, status=status.HTTP_200_OK)
 
 @csrf_exempt
 @api_view(['POST'])
@@ -37,7 +37,7 @@ def addProduct(request):
             'data': serializer.data,
             'error': False
         }
-        return Response(result)
+        return Response(result, status=status.HTTP_201_CREATED)
     else:
         result = {
             'code': status.HTTP_400_BAD_REQUEST,
@@ -45,7 +45,7 @@ def addProduct(request):
             'data': [],
             'error': True
         }
-        return Response(result)
+        return Response(result, status=status.HTTP_400_BAD_REQUEST)
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
@@ -59,7 +59,7 @@ def product(request):
             'data': serializer.data,
             'error': False
         }
-        return Response(result)
+        return Response(result, status=status.HTTP_200_OK)
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
         if(serializer.is_valid()):
@@ -78,4 +78,88 @@ def product(request):
                 'data': [],
                 'error': True
             }
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductList(APIView):
+    def get(self, request, format=None):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        result = {
+            'code': status.HTTP_200_OK,
+            'message': 'success',
+            'data': serializer.data,
+            'error': False
+        }
+        return Response(result)
+
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            result = {
+                'code': status.HTTP_200_OK,
+                'message': 'success',
+                'data': serializer.data,
+                'error': False
+            }
             return Response(result)
+        else:
+            result = {
+                'code': status.HTTP_400_BAD_REQUEST,
+                'message': serializer.errors,
+                'data': [],
+                'error': True
+            }
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+
+class ProductDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise Http404
+            #TODO: return result data empty
+
+    def get(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        result = {
+            'code': status.HTTP_200_OK,
+            'message': 'success',
+            'data': serializer.data,
+            'error': False
+        }
+        return Response(result, status=status.HTTP_200_OK) 
+
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            result = {
+                'code': status.HTTP_200_OK,
+                'message': 'success',
+                'data': serializer.data,
+                'error': False
+            }
+            return Response(result, status=status.HTTP_200_OK)
+        else:
+            result = {
+                'code': status.HTTP_400_BAD_REQUEST,
+                'message': serializer.errors,
+                'data': [],
+                'error': True
+            }
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        product = self.get_object(pk)
+        product.delete()
+        result = {
+            'code': status.HTTP_204_NO_CONTENT,
+            'message': 'success',
+            'data': [],
+            'error': False
+        }
+        return Response(result, status=status.HTTP_204_NO_CONTENT)
